b55abf345d6019c85013a9ccecd77d2ffdca671f
abolajii <abolajiadeajayi@gmail.com>
2024-10-30 08:30:29 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hello, JavaScript with Express on Vercel!" });
+});
+
+// Export the app for Vercel's serverless functions
+module.exports = app;
